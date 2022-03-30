8b087fd5b41aabf480a55699f00a9ca07754816a
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-30 14:42:42 -0700

    added routes to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Auth from './views/Auth/Auth';
+import Home from './views/Home/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path={'/todos'}>
+            <Home />
+          </Route>
+          <Route exact path={'/'}>
+            <Auth />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
